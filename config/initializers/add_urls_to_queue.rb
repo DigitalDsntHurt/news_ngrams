dde4e5388ff2ce276f9bb17961688b78713e3c9d
N Smith <digitaldoesnthurt@gmail.com>
2018-02-06 23:10:42 -0800

    prevent rufus jobs from running on initialization of rails console PLUS changed over to rufus singleton

--- config/initializers/add_urls_to_queue.rb
+++ config/initializers/add_urls_to_queue.rb
@@ -1,4 +1,6 @@
-scheduler = Rufus::Scheduler.new
+### 
+### ### Use this scheduler for adding urls to queue
+###
 
 def get_stuff_from_page(page,xpath)
 	begin
@@ -16,38 +18,42 @@ def get_stuff_from_page(page,xpath)
 	end
 end
 
-### 
-### ### Use this scheduler for adding urls to queue
-###
-#scheduler.ever '2s' do
-scheduler.every '20m', :first_in => '2s' do
+unless defined?(Rails::Console)
 
-	## FOX NEWS
-	pub_name = "fox news"
-	homepage = "http://www.foxnews.com/"
-	xpath = "//*[@class='title']/a/@href"
+	#scheduler = Rufus::Scheduler.new
+	scheduler = Rufus::Scheduler.singleton
 
-	#links = []
-	homepage_article_links = get_stuff_from_page(homepage,xpath).reject{|link| link.include?("video.fox")}.reject{|link| link.length < 20}.reject{|link| link.include?("radio.foxnews.com")}.map{|url| (url.start_with?("//") ? url[2..-1] : url) }#.reject{|link| link.include?("")}
-	homepage_article_links.uniq.each{|link|
-		puts "processing #{link}"
-		unless UrlQueue.where(:url => link).count > 0
-			UrlQueue.create!(:date_added_to_queue => Date.today, :publication => pub_name, :url => link)
-		end
-	}
 
-	## DRUDGE REPORT
-	pub_name = "drudge report"
-	homepage = "http://www.drudgereport.com/"
-	xpath = "//a/@href"
+	#scheduler.ever '2s' do
+	scheduler.every '20m', :first_in => '2s' do
 
-	homepage_article_links = get_stuff_from_page(homepage,xpath).reject{ |link| link.each_char.count < 65 }
-	homepage_article_links.uniq.each{|link|
-		unless UrlQueue.where(:url => link).count > 0
-			UrlQueue.create!(:date_added_to_queue => Date.today, :publication => pub_name, :url => link)
-		end
-	}
-	
+		## FOX NEWS
+		pub_name = "fox news"
+		homepage = "http://www.foxnews.com/"
+		xpath = "//*[@class='title']/a/@href"
 
-	puts "\n\n\n ~*~*~ \nDone adding links to URLQueue\n ~*~*~ \n\n\n"
+		#links = []
+		homepage_article_links = get_stuff_from_page(homepage,xpath).reject{|link| link.include?("video.fox")}.reject{|link| link.length < 20}.reject{|link| link.include?("radio.foxnews.com")}.map{|url| (url.start_with?("//") ? url[2..-1] : url) }#.reject{|link| link.include?("")}
+		homepage_article_links.uniq.each{|link|
+			puts "processing #{link}"
+			unless UrlQueue.where(:url => link).count > 0
+				UrlQueue.create!(:date_added_to_queue => Date.today, :publication => pub_name, :url => link)
+			end
+		}
+
+		## DRUDGE REPORT
+		pub_name = "drudge report"
+		homepage = "http://www.drudgereport.com/"
+		xpath = "//a/@href"
+
+		homepage_article_links = get_stuff_from_page(homepage,xpath).reject{ |link| link.each_char.count < 65 }
+		homepage_article_links.uniq.each{|link|
+			unless UrlQueue.where(:url => link).count > 0
+				UrlQueue.create!(:date_added_to_queue => Date.today, :publication => pub_name, :url => link)
+			end
+		}
+		
+
+		puts "\n\n\n ~*~*~ \nDone adding links to URLQueue\n ~*~*~ \n\n\n"
+	end
 end
