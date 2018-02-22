a51ae90ccbf75da08d922249ff1db4ad8e5e443b
N Smith <digitaldoesnthurt@gmail.com>
2018-02-21 23:05:36 -0800

    scary db backups fuckups

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122040709) do
+ActiveRecord::Schema.define(version: 20180222062705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ngrams", force: :cascade do |t|
+  create_table "ngrams", id: :serial, force: :cascade do |t|
     t.date     "date"
     t.string   "n"
     t.string   "terms"
@@ -24,34 +24,7 @@ ActiveRecord::Schema.define(version: 20180122040709) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "seed_flats", force: :cascade do |t|
-    t.date     "started_date"
-    t.string   "crop"
-    t.string   "crop_variety"
-    t.string   "seed_brand"
-    t.string   "medium"
-    t.string   "format"
-    t.float    "seed_weight"
-    t.string   "seed_wt_units"
-    t.string   "seed_media_treatment_notes"
-    t.date     "first_emerge_date"
-    t.date     "full_emerge_date"
-    t.string   "emergence_notes"
-    t.date     "date_of_first_transplant"
-    t.string   "first_transplant_notes"
-    t.date     "date_of_second_transplant"
-    t.string   "second_transplant_notes"
-    t.date     "harvested_on"
-    t.float    "harvest_weight_oz"
-    t.float    "hrvst_wt_lbs"
-    t.integer  "harvest_week"
-    t.string   "harvest_notes"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "flat_id"
-  end
-
-  create_table "url_queues", force: :cascade do |t|
+  create_table "url_queues", id: :serial, force: :cascade do |t|
     t.date     "date_added_to_queue"
     t.string   "publication"
     t.string   "url"
